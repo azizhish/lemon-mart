d1451965cd8c206cb7dd3552caa9c8beb324417f
Hisham Aziz <azizhish@gmail.com>
2018-07-28 17:19:32 +0400

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <mat-toolbar color="primary">
+      <a mat-button routerLink="/home"><h1>LemonMart</h1></a>
+    </mat-toolbar>
+    <router-outlet></router-outlet>
+  `,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'lemon-mart';
+  title = 'lemon-mart'
 }
