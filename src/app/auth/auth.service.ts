5acf40adbcfe56bbb50694dda878e84b50b8ad10
Hisham Aziz <azizhish@gmail.com>
2018-09-04 07:33:07 +0400

    cont auth

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -11,7 +11,64 @@ import { Role } from './role.enum'
   providedIn: 'root',
 })
 export class AuthService {
-  constructor() {}
+  private readonly authProvider: (
+    email: string,
+    password: string
+  ) => Observable<IServerAuthResponse>
+
+  authStatus = new BehaviorSubject<IAuthStatus>(defaultAuthStatus)
+
+  constructor(private httpClient: HttpClient) {
+    // Fake login function to simulate roles
+    this.authProvider = this.fakeAuthProvider
+    // Example of a real login call to server-side
+    // this.authProvider = this.exampleAuthProvider
+  }
+  login(email: string, password: string): Observable<IAuthStatus> {
+    this.logout()
+    const loginResponse = this.authProvider(email, password).pipe(
+      map(value => {
+        return decode(value.accessToken) as IAuthStatus
+      }),
+      catchError(transformError)
+    )
+    loginResponse.subscribe(
+      res => {
+        this.authStatus.next(res)
+      },
+      err => {
+        this.logout()
+        return observableThrowError(err)
+      }
+    )
+    return loginResponse
+  }
+  private fakeAuthProvider(
+    email: string,
+    password: string
+  ): Observable<IServerAuthResponse> {
+    if (!email.toLowerCase().endsWith('@test.com')) {
+      return observableThrowError('Failed to login! Email needs to end with @test.com.')
+    }
+    const authStatus = {
+      isAuthenticated: true,
+      userId: 'e4d1bc2ab25c',
+      userRole: email.toLowerCase().includes('cashier')
+        ? Role.Cashier
+        : email.toLowerCase().includes('clerk')
+          ? Role.Clerk
+          : email.toLowerCase().includes('manager')
+            ? Role.Manager
+            : Role.None,
+    } as IAuthStatus
+    const authResponse = {
+      accessToken: sign(authStatus, 'secret', {
+        expiresIn: '1h',
+        algorithm: 'none',
+      }),
+    } as IServerAuthResponse
+    return of(authResponse)
+  }
 }
 
 export interface IAuthStatus {
